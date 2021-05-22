3bc69bc537b2642609af37e44368c52cb5015376
douruanliang <douruanliang@meishubao.com>
2021-05-22 08:05:54 +0800

    商品微服务-brand

--- shopx-service/shopx-service-goods/src/main/java/com/shopx/GoodsApplication.java
+++ shopx-service/shopx-service-goods/src/main/java/com/shopx/GoodsApplication.java
@@ -3,12 +3,14 @@ package com.shopx;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  *
  */
 @SpringBootApplication
 @EnableEurekaClient
+@MapperScan(basePackages ={"com.shopx.dao"})
 public class GoodsApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsApplication.class,args);
